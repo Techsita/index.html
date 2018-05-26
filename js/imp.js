fa2d5cb23caa264d95300199dc9407858ff7ecf3
Techsita <danhite@me.com>
2018-05-26 02:15:29 -0700

    Update imp.js

--- js/imp.js
+++ js/imp.js
@@ -1,4 +1,4 @@
 // window.self.document.title="was here";
 import {myobject} from './exp.js';
 // export default var xxx = 'foo';
-window.self["obj"] = "" + typeof myobject;
+window.self["obj"] = myobject;
