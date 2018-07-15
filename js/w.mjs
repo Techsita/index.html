48df890ac935241053f0919e5c96163ac42f6dce
Dan Hite <danhite@me.com>
2018-07-14 23:37:53 -0700

    Update w.mjs

--- js/w.mjs
+++ js/w.mjs
@@ -1,3 +1,7 @@
-onmessage = function(e) {
-  postMessage(String(e.data)+typeof self['eval']);
+onmessage = function mess(e) {
+  try { 
+    postMessage(String(e.data) + typeof eval); 
+  } catch(err) {
+    postMessage(err.message);
+  }
 }
