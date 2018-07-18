3cb87f707b5f2d9d758e5fd260b2eb3dc0c2a7a6
Dan Hite <danhite@me.com>
2018-07-17 21:51:05 -0700

    Create son.css

---
+++ son.css
@@ -0,0 +1,23 @@
+<!--
+// /*
+"po use strict"; // bug off block for css parser, welcome js...
+`this is a js ~ignored multi-line string...
+// let's do some (if) css... */
+{}
+ body { background-color: pink; } /* css is rosy, js who cares? */
+/* ` ; true === true // js yes, css no; could end css like this
+// but lets assume js wants to go write along to html...
+/* turn off js also; restore (if) html...
+-->
+<head><title>tripleCacheMimeless</title>
+<link href="/index.html/son.css" rel="stylesheet">
+</head><body>body
+<script src="/index.html/holyghost.js"></script>
+</body><!--
+*/
+{}
+/* out of comment and back in for both js & css... */
+// /*
+;
+document.title="peekaboo!"; 
+/* and finis for both js & css... html will cope fine */
