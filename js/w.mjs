5eeae9e3d5809b99a9892bf8cb286df191d3ed81
Dan Hite <danhite@me.com>
2018-07-18 22:04:05 -0700

    Update w.mjs

--- js/w.mjs
+++ js/w.mjs
@@ -5,4 +5,4 @@ self.onmessage = function mess(e) {
     postMessage(String(e.data) + " // " + err.message);
   }
 }
-self.importScripts("data:text/javascript;/self.js?#,function%20evil(doer){try{return(eval(doer));}catch(e){return(e);}");
+self.importScripts("data:text/javascript;/self.js?#,function%20evil(doer){try{return(eval(doer));}catch(e){return(e);}}");
