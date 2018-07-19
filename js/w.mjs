f0b9bcce3107881301469471af19c382afd0ae43
Dan Hite <danhite@me.com>
2018-07-19 01:21:00 -0700

    Update w.mjs

--- js/w.mjs
+++ js/w.mjs
@@ -1,16 +1,16 @@
 let url = import.meta.url;
 
+export let stuff = {url: url, foo: {bar: 666}};
+
 // self = self || {closed: true};
 
 self.onmessage = function mess(e) {
   try { 
-    postMessage(String(e.data) + " // " + String(eval(e.data)) ); 
+    self.postMessage(String(e.data) + " // " + String(eval(e.data)) ); 
   } catch(err) {
-    postMessage(String(e.data) + " // " + err.message);
+    self.postMessage(String(e.data) + " // " + err.message);
   }
 }
 // self.importScripts("data:text/javascript;/self.js?#,function%20evil(doer){try{return(eval(doer));}catch(e){return(e);}}");
-
-export let stuff = {url: url, foo: {bar: 666}};
 
 export default function evil(doer){try{return(eval(doer));}catch(e){return(e);}}
