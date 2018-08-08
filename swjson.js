fca66785f833988953b0b76fbc7b333ed4639652
Dan Hite <danhite@me.com>
2018-08-07 21:01:43 -0700

    Update swjson.js

--- swjson.js
+++ swjson.js
@@ -1,3 +1,13 @@
+let isStrict = ( function () { return !!!this } ) (); 
+
+self.onmessage = function mess(e) {
+  try { 
+    self.postMessage(String(e.data) + " // " + String(eval(e.data)) ); 
+  } catch(err) {
+    self.postMessage(String(e.data) + " // " + err.message);
+  }
+}
+
 self.addEventListener('install', function(event) {
   self.skipWaiting();
 });
