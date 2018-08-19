5cde01d50ab750fe297bd6bc100c61321c1091be
Dan Hite <danhite@me.com>
2018-08-19 13:31:18 -0700

    Update we.mjs
    WIP still

--- js/we.mjs
+++ js/we.mjs
@@ -2,7 +2,7 @@
 let isStrict = ( function () { return Object.is(undefined, this) } ) (); 
 
 let eports = new Set();
-let portEval = portEval0;
+let portEval = portEval1;
 
 self.onmessage = portEval;
 
@@ -20,5 +20,31 @@ function portEval0 (e) {
     this.postMessage(String(e.data) + " // " + String(eval(e.data)) ); 
   } catch(err) {
     this.postMessage(String(e.data) + " // " + err.message);
+  }
+}
+
+function portEval1 (e) {
+  portal(e.ports[0]);
+  
+  let ev = {};
+  try { 
+    ev = eval(e.data); 
+  } catch(err) {
+    ev = { 
+      data: e.data ,
+      name: err.name ,
+      message: err.message
+    }
+  }
+  
+  try {
+    this.postMessage(ev);
+  } catch (err) {
+    ev = { 
+      data: e.data ,
+      name: err.name ,
+      message: err.message
+    }
+    this.postMessage(ev);
   }
 }
