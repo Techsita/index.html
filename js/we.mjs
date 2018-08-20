3360b282e2ccde4b48a6cf95f537c63c07388dbc
Dan Hite <danhite@me.com>
2018-08-20 16:01:34 -0700

    Update we.mjs

--- js/we.mjs
+++ js/we.mjs
@@ -7,7 +7,7 @@ console.log = self.log.push.bind(self.log);
 let eports = new Set(); // hold messchan refs, if needed
 let portEval = portEval1; // dev eval ala mode
 
-self.onmessage = portEval; // opt-in for win, worker, service worker
+self.onmessage = portEval; // dev eval opt-in for us, whether: win, worker, service worker
 
 function portal(oport) {
   if (oport && oport instanceof MessagePort) {
@@ -17,19 +17,18 @@ function portal(oport) {
 }
 
 function portEval1 (e) { // NB ignoring all security...
+  if (e.ports.length) { // quickie version of tranferable(s) processing, as one new eval mc
+    portal(e.ports[0]);
+  } 
+  
   let ev = {};
-  if (e.ports.length) { // quickie version of tranferables processing vs eval
-    portal(e.ports[0]); // nop if, eg, []
-    ev = e.data; // no eval, idemppotent (also, eg, result output)
-  } else { // eval
-    try { // dev eval, caveat emptor
-      ev = eval(e.data); 
-    } catch(err) {
-      ev = { 
-        data: e.data ,
-        name: err.name ,
-        message: err.message
-      }
+  try { // dev eval, caveat emptor
+    ev = eval(e.data); 
+  } catch(err) {
+    ev = { 
+      data: e.data ,
+      name: err.name ,
+      message: err.message
     }
   }
   
@@ -37,30 +36,34 @@ function portEval1 (e) { // NB ignoring all security...
     let replyto = null;
     let pmflag = null;
     
-    if (e.source && e.source instanceof Object && e.source.postMessage && e.source.postMessage instanceof Function) { // if messchan source === null
-        replyto = e.source;
+    if (e.source && e.source instanceof Object && e.source.postMessage && e.source.postMessage instanceof Function) { // 
+        replyto = e.source; // fyi, we're not a mc... source on win, sw (~fake as ClientWindow)
     } else {
       if (this && this instanceof Object && this.postMessage && this.postMessage instanceof Function) { // this messchan or fallback
-        replyto = this;
-      } else { // to-who lookup foo
+        replyto = this; // worker self this, or mc this (avoided this Mo for win or sw due to misc. awkward issues)
+      } else { // to-who lookup foo, could complain, if useful to do so, eh?
         replyto = null;
       }
     }
     if (replyto) pmflag = (2 === replyto.postMessage.length); // diff pm versions, extra req parameter foo
     
-    if ( replyto && pmflag ) {
-      if (replyto === self) { // pmflag laziness, ie implies assert we are Window, not worker or channel
-        self.status = (e.data instanceof Object)? JSON.stringify(e.data) : String(e.data); 
-        self.status += " // ";
-        self.status += (ev instanceof Object)? JSON.stringify(ev) : String(ev); 
+    if ( replyto && pmflag ) { // win, so po no reply at all (no recurse risk)...
+      if (e.ports.length) { // quickie version of if got tranferable port, then mc initial eval replyto
+          e.ports[0].postMessage(ev); // to (other?) window sender, done.
       } else {
-        replyto.postMessage(ev, '*', [new ArrayBuffer(0)]); // reply to window sender, ignoring security origin, no eval requested
+        if (replyto === self) { // pmflag laziness, ie implies can assert we are Window, not s/worker or channel
+          self.status = (e.data instanceof Object)? JSON.stringify(e.data) : String(e.data); 
+          self.status += " // ";
+          self.status += (ev instanceof Object)? JSON.stringify(ev) : String(ev); 
+        } else {
+          // other win sans mc, no reply, as it's nicer to avoid recurse
+        }
       }
-    } else {
+    } else { // not pmflag, so "normal" pm, no recurse risk unless was stupid in chaining mc
       if (replyto) {
         replyto.postMessage(ev);
-      } else {
-        console.log(ev); // foo so just log result
+      } else {  // foo so just log result...
+        console.log(ev);
       }
     }
     
