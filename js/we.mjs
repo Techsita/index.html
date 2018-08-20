cfec4d3643e5edbcd5c5a145cd1d5f8743854929
Dan Hite <danhite@me.com>
2018-08-20 00:51:23 -0700

    Update we.mjs

--- js/we.mjs
+++ js/we.mjs
@@ -30,28 +30,34 @@ function portEval1 (e) { // NB ignoring all security...
     }
   }
   
-  try { // reply to sender, ignoring security origin
+  try { 
     let replyto = null;
-    if (e.source && e.source instanceof Object) { // if messchan source === null
+    if (e.source && e.source instanceof Object && e.source.postMessage && e.source.postMessage instanceof Function) { // if messchan source === null
         replyto = e.source;
     } else {
-      if (this && this instanceof Object) { // this messchan or fallback
+      if (this && this instanceof Object && this.postMessage && this.postMessage instanceof Function) { // this messchan or fallback
         replyto = this;
       } else { // to-who lookup foo
         replyto = null;
       }
     }
-    if ( (replyto === self) && ("Window" === Object.getPrototypeOf(self).constructor.name) ) {
-      self.status = (e.data instanceof Object)? JSON.stringify(e.data) : String(e.data); 
-      self.status += " // ";
-      self.status += (ev instanceof Object)? JSON.stringify(ev) : String(ev);  
+    
+    if ( replyto && ("Window" === Object.getPrototypeOf(replyto).constructor.name) ) {
+      if (replyto === self) {
+        self.status = (e.data instanceof Object)? JSON.stringify(e.data) : String(e.data); 
+        self.status += " // ";
+        self.status += (ev instanceof Object)? JSON.stringify(ev) : String(ev); 
+      } else {
+        replyto.postMessage(ev, '*'); // reply to sender, ignoring security origin
+      }
     } else {
-      if (replyto && replyto.postMessage && replyto.postMessage instanceof Function) {
+      if (replyto) {
         replyto.postMessage(ev);
       } else {
         console.log(ev); // foo so just log result
       }
     }
+    
   } catch (err) {
     ev = { 
       data: e.data ,
